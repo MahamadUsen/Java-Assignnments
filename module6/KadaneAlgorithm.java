d3016e3da405003214b081865096f2b5424705ab
MahamadUsen <husentg2202@gmail.com>
2026-03-09 20:27:34 +0530

    Create KadaneAlgorithm.java

---
+++ module6/KadaneAlgorithm.java
@@ -0,0 +1,28 @@
+public class KadaneAlgorithm {
+
+    public static int maxSubArraySum(int[] arr) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            currentSum += arr[i];
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int result = maxSubArraySum(arr);
+        System.out.println("Maximum Subarray Sum: " + result);
+    }
+}
